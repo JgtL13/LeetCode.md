493b47f0911170f6a5b6247bf3fd64e21c049c59
Justin Lin <justin.jgtl@gmail.com>
2022-06-07 16:28:50 +0800

    Scrape content from english website working

--- leetcode_markdown.py
+++ leetcode_markdown.py
@@ -1,296 +1,138 @@
-# -*- coding: UTF-8 -*-
-import os
-import requests, json
-import re
+import requests,json,re
+from requests_toolbelt import MultipartEncoder
 
-# 获取题目的方法来自下面的博客，并且添加了一些修改来更好地符合个人需要。
-# https://wanakiki.github.io/2020/leetcode-spider/
-Remove = ["</?p>", "</?ul>", "</?ol>", "</li>", "</sup>"]
-Replace = [["&nbsp;", " "], ["&quot;", '"'], ["&lt;", "<"], ["&gt;", ">"],
-           ["&le;", "≤"], ["&ge;", "≥"], ["<sup>", "^"], ["&#39", "'"],
-           ["&times;", "x"], ["&ldquo;", "“"], ["&rdquo;", "”"],
-           [" *<strong> *", " **"], [" *</strong> *", "** "],
-           [" *<code> *", " `"], [" *</code> *", "` "], ["<pre>", "```\n"],
-           ["</pre>", "\n```\n"], ["<em> *</em>", ""], [" *<em> *", " *"],
-           [" *</em> *", "* "], ["</?div.*?>", ""], ["	*</?li>", "- "]]
+session = requests.Session()
+user_agent = r'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
 
 
-def convert(src):
-    # pre内部预处理
-    def remove_label_in_pre(matched):
-        tmp = matched.group()
-        tmp = re.sub("<[^>p]*>", "", tmp)  # 不匹配>与p
-        return tmp
+def login(username, password):
+    url = 'https://leetcode.com'
+    cookies = session.get(url).cookies
+    for cookie in cookies:
+        if cookie.name == 'csrftoken':
+            csrftoken = cookie.value
 
-    src = re.sub("<pre>[\s\S]*?</pre>", remove_label_in_pre,
-                 src)  # 注意此处非贪心匹配，因为可能有多个示例
-    # 可以直接删除的标签
-    for curPattern in Remove:
-        src = re.sub(curPattern, "", src)
-    # 需要替换内容的标签
-    for curPattern, curRepl in Replace:
-        src = re.sub(curPattern, curRepl, src)
-    return src
-
-
-def get_today_name() -> str:
-    """
-        获取今天的每日一题的 slug
-    """
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
-    session = requests.Session()
-    url = "https://leetcode-cn.com/graphql"
-    params = {
-        'oprationName':
-        "questionOfToday",
-        'query':
-        '''
-        query questionOfToday {
-            todayRecord {
-                question {
-                    questionFrontendId
-                    questionTitleSlug
-                    __typename
-                }
-                lastSubmission {
-                    id
-                    __typename
-                }
-                date
-                userStatus
-                __typename
-            }
-        }
-        '''
+    url = "https://leetcode.com/accounts/login"
+        
+    params_data = {
+        'csrfmiddlewaretoken': csrftoken,
+        'login': username,
+        'password':password,
+        'next': 'problems'
     }
-    json_data = json.dumps(params).encode('utf8')
-    headers = {
-        'User-Agent': user_agent,
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/json',
-        'Referer': 'https://leetcode-cn.com/problemset/all/'
-    }
-    resp = session.post(url, data=json_data, headers=headers, timeout=10)
-    resp.encoding = 'utf8'
-    content = resp.json()
-    # 题目详细信息
-    # print(content)
-    question = content['data']['todayRecord'][0]['question']
-    return question['questionTitleSlug']
+    headers = {'User-Agent': user_agent, 'Connection': 'keep-alive', 'Referer':         'https://leetcode.com/accounts/login/',
+        "origin": "https://leetcode.com"}
+    m = MultipartEncoder(params_data)   
 
+    headers['Content-Type'] = m.content_type
+    session.post(url, headers = headers, data = m, timeout = 10, allow_redirects = False)
+    is_login = session.cookies.get('LEETCODE_SESSION') != None
+    return is_login
 
-def get_today_url():
-    return "https://leetcode-cn.com/problems/" + get_today_name
+def get_problems():
+    url = "https://leetcode.com/api/problems/all/"
 
+    
+    headers = {'User-Agent': user_agent, 'Connection': 'keep-alive'}
+    resp = session.get(url, headers = headers, timeout = 10)
+       
+    question_list = json.loads(resp.content.decode('utf-8'))
 
-def get_all(slug: str) -> dict:
-    """根据题目的 id 获取题目的名字，内容，代码块，标签，返回 json 格式的 dict 对象
-    """
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
-    session = requests.Session()
-    url = "https://leetcode-cn.com/graphql"
-    params = {
-        'operationName':
-        "getQuestionDetail",
-        'variables': {
-            'titleSlug': slug
-        },
-        'query':
-        '''query getQuestionDetail($titleSlug: String!) {
+    for question in question_list['stat_status_pairs']:
+        # 题目编号
+        question_id = question['stat']['question_id']
+        # 题目名称
+        question_slug = question['stat']['question__title_slug']
+        # 题目状态
+        question_status = question['status']
+
+        # 题目难度级别，1 为简单，2 为中等，3 为困难
+        level = question['difficulty']['level']
+
+        # 是否为付费题目
+        if question['paid_only']:
+            continue
+        print(question_slug)      
+
+def get_problem_by_slug(slug):
+    url = "https://leetcode.com/graphql"
+    params = {'operationName': "getQuestionDetail",
+        'variables': {'titleSlug': slug},
+        'query': '''query getQuestionDetail($titleSlug: String!) {
             question(titleSlug: $titleSlug) {
                 questionId
                 questionFrontendId
-                title
-                titleSlug
+                questionTitle
+                questionTitleSlug
                 content
-                translatedTitle
-                translatedContent
                 difficulty
+                stats
+                similarQuestions
+                categoryTitle
                 topicTags {
-                    name
-                    slug
-                    translatedName
-                    __typename
+                        name
+                        slug
                 }
-                codeSnippets {
+            }
+        }'''
+    }
+
+    json_data = json.dumps(params).encode('utf8')
+                        
+    headers = {'User-Agent': user_agent, 'Connection': 
+        'keep-alive', 'Content-Type': 'application/json',
+        'Referer': 'https://leetcode.com/problems/' + slug}
+    resp = session.post(url, data = json_data, headers = headers, timeout = 10)
+    content = resp.json()
+
+    # 题目详细信息
+    question = content['data']['question']
+    print(question)
+
+def get_submissions(slug):
+    url = "https://leetcode.com/graphql"
+    params = {'operationName': "Submissions",
+        'variables':{"offset":0, "limit":20, "lastKey": '', "questionSlug": slug},
+            'query': '''query Submissions($offset: Int!, $limit: Int!, $lastKey: String, $questionSlug: String!) {
+                submissionList(offset: $offset, limit: $limit, lastKey: $lastKey, questionSlug: $questionSlug) {
+                lastKey
+                hasNext
+                submissions {
+                    id
+                    statusDisplay
                     lang
-                    langSlug
-                    code
+                    runtime
+                    timestamp
+                    url
+                    isPending
                     __typename
                 }
                 __typename
             }
         }'''
     }
+
     json_data = json.dumps(params).encode('utf8')
-    headers = {
-        'User-Agent': user_agent,
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/json',
-        'Referer': 'https://leetcode-cn.com/problems/' + slug
-    }
-    resp = session.post(url, data=json_data, headers=headers, timeout=10)
-    resp.encoding = 'utf8'
+
+    headers = {'User-Agent': user_agent, 'Connection': 'keep-alive', 'Referer': 'https://leetcode.com/accounts/login/',
+        "Content-Type": "application/json"}  
+    resp = session.post(url, data = json_data, headers = headers, timeout = 10)
     content = resp.json()
-    # 题目详细信息
-    # print(content)
-    question = content['data']['question']
-    return question
+    for submission in content['data']['submissionList']['submissions']:
+        print(submission)
 
+def get_submission_by_id(submission_id):
+    url = "https://leetcode.com/submissions/detail/" + submission_id
+    headers = {'User-Agent': user_agent, 'Connection': 'keep-alive', "Content-Type": "application/json"}
+    code_content = session.get(url, headers = headers, timeout = 10)
 
-def get_problem_content(slug: str) -> str:
-    question = get_all(slug)
-    res = convert(question['translatedContent'])
-    # 在正文后面填上标签
-    res += "\n \n**标签**\n"
-    tags = question['topicTags']
-    for tag in tags:
-        if tag['translatedName'] != None:
-            tagName = tag['translatedName']
-        else:
-            tagName = tag['name']
-        res += "`" + tagName + "` "
+    pattern = re.compile(r'submissionCode: \'(?P<code>.*)\',\n  editCodeUrl', re.S)
+    m1 = pattern.search(code_content.text)
+    code = m1.groupdict()['code'] if m1 else None
+    print(code)
 
-    return res + "\n"
-
-
-def get_solution_by_lang(slug: str, lang: str) -> str:
-    """
-        获取给定题目的对应语言的函数
-
-        支持的参数如下
-
-        C++ Java Python Python3 C C# JavaScript Ruby Swift Go Scala Kotlin
-        Rust PHP TypeScript Racket
-    """
-    question = get_all(slug)
-    # 获取对应语言的函数
-    codeSnippets = question['codeSnippets']
-    for x in codeSnippets:
-        if x['lang'] == lang:
-            return x['code']
-
-
-def gen_markdown(path, content, title, url):
-    """ 生成 markdown 文件
-    """
-    file = open(path, 'a', encoding="utf-8")
-    markdowncontenct = """# {titlename}
-[{Url}]({Url}) 
-## 原题
-{Content}
-
-## 
-```go
-
-```
->
-""".format(titlename=title, Url=url, Content=content)
-    file.write(markdowncontenct)
-    print(path)
-    file.close()
-
-
-def gen_gofile(path: str, title: str, url: str, func: str) -> None:
-    """ 生成 solution_test.go 文件
-    
-    """
-    file = open(path, 'a', encoding="utf-8")
-    go_content = """package leetcode
-
-import (
-	"reflect"
-	"testing"
-)
-
-// {0}
-// {1}
-{2}
-func TestSolution(t *testing.T) {{
-	testCases := []struct {{
-		desc string
-		want 
-	}}{{
-		{{
-            want: ,
-		}},
-	}}
-	for _, tC := range testCases {{
-		t.Run(tC.desc, func(t *testing.T) {{
-			get := 
-			if !reflect.DeepEqual(tC.want,get){{
-				t.Errorf("input: %+v get: %v\\n",tC,get)
-			}}
-		}})
-	}}
-}}
-""".format(title.replace(" ", ""), url, func)
-    file.write(go_content)
-    print(path)
-    file.close()
-
-
-def gen_go_mod(path: str) -> None:
-    """
-        生成 go.mod 文件
-    """
-    file = open(path, 'a', encoding="utf-8")
-    file.write("module leetcode\ngo 1.15")
-    file.close()
-
-
-def gen_files(url: str):
-    """
-        根据 url 生成对应的文件
-
-        当 url 为空时自动爬取每日一题
-        当 url 不为空时爬取对应的题目
-    """
-    if url.startswith("https://leetcode-cn.com/problems/"):
-        slug = url.replace("https://leetcode-cn.com/problems/", "",
-                           1).strip('/')
-    elif len(url) > 0:
-        print(
-            "Wrong URL ! Please Check\n.It should be like https://leetcode-cn.com/problems/evaluate-division/"
-        )
-    else:
-        slug = get_today_name()
-    url = "https://leetcode-cn.com/problems/" + slug
-    question = get_all(slug=slug)
-    title = question['questionFrontendId'] + '.' + question['translatedTitle']
-
-    content = get_problem_content(slug)
-
-    func = get_solution_by_lang(slug, 'Go')
-    content = re.sub(r'\n\n\n\n*', "\n", content)  # 替换掉多个换行符
-
-    base_dir = os.getcwd()
-    newfolder = os.path.join(base_dir,
-                             title.replace(". ", ".").replace(" ", "-"))
-    if not os.path.exists(newfolder):
-        os.makedirs(newfolder)
-        print("create new folder ", newfolder)
-    else:
-        print("already exist folder:", newfolder)
-
-    # 生成 markdown 文件
-    filepath = os.path.join(base_dir, newfolder, "README.md")
-    gen_markdown(filepath, content, title, url)
-
-    # 生成 solution_test.go 文件
-    filepath = os.path.join(base_dir, newfolder, "solution_test.go")
-    gen_gofile(filepath, title, url, func)
-
-    # 生成 go.mod 文件
-    filepath = os.path.join(base_dir, newfolder, "go.mod")
-    gen_go_mod(filepath)
-    return
-
-
-if __name__ == '__main__':
-    # 只用修改这个 url 即可
-    # 比如题目链接为 https://leetcode-cn.com/problems/evaluate-division/ 就直接全部复制即可
-    # 当 url 为空的时候爬取每日一题
-    url = ""
-
-    gen_files(url=url)
+#print(login('u', 'p'))
+# get_problems()
+get_problem_by_slug('two-sum')
+#get_submissions('two-sum')
+#get_submission_by_id('')
